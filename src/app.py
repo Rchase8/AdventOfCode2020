d8e1ce7116ade1e12d4ea7ca2d4624d98ce473f2
Rchase8 <chaseerobert@gmail.com>
2020-12-05 22:27:43 +0000

    made it sort through numbers

--- src/app.py
+++ src/app.py
@@ -2,13 +2,38 @@ def get_number_list():
   file = open("Data/1_1inputs.txt", "r")
   lines = file.readlines()
 
-  numberlist = []
+  new_numberlist = []
   for line in lines:
-    numberlist.append(int(line))
-    
-  return numberlist
+    new_numberlist.append(int(line))
+
+  return new_numberlist
+# block ends
+
+def filter_number_list(the_numberlist, criteria_def):
+  for first_pos in range(0,len(the_numberlist)):
+    for second_pos in range(0,len(the_numberlist)):
+      if first_pos == second_pos:
+        continue
+
+      first_number = the_numberlist[first_pos]
+      second_number = the_numberlist[second_pos]
 
 
+
+
+      print(first_number, second_number)
+      pass
+  pass
+# block ends
+
+def is_sum_2020(the_first_number, the_second_number):
+  pass
+# block ends
+
+# main
 numberlist = get_number_list()
 
 print(numberlist)
+
+filter_number_list(numberlist, is_sum_2020)
+
