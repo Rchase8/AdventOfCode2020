d2d98825177b3ff3b0eece694505b9b36ffd5b04
Rchase8 <chaseerobert@gmail.com>
2020-12-05 23:41:36 +0000

    finishd :)

--- src/app.py
+++ src/app.py
@@ -16,16 +16,32 @@ def filter_number_list(the_numberlist, criteria_def):
       if first_pos == second_pos:
         continue
 
-      first_number = the_numberlist[first_pos]
-      second_number = the_numberlist[second_pos]
-      is_true = criteria_def(first_number, second_number)
+      if the_numberlist[first_pos] + the_numberlist[second_pos] > 2020:
+        continue
 
-      print(first_number, second_number, is_true)
+      for third_pos in range(0,len(the_numberlist)):
+        if second_pos == third_pos:
+          continue
 
-      if is_true: 
-        filtered_number_list.append(first_number)
-        filtered_number_list.append(second_number)
-        break
+        if first_pos == third_pos:
+          continue
+        
+
+        first_number = the_numberlist[first_pos]
+        second_number = the_numberlist[second_pos]
+        third_number = the_numberlist[third_pos]
+        is_true = criteria_def(first_number, second_number, third_number)
+
+        print(first_number, second_number, third_number, is_true)
+
+        if is_true: 
+          filtered_number_list.append(first_number)
+          filtered_number_list.append(second_number)
+          filtered_number_list.append(third_number)
+          break
+        # end of 3rd for loop 
+        if len(filtered_number_list) > 0:
+          break
       # end of 2nd for Loop
     if len(filtered_number_list) > 0:
       break
@@ -35,8 +51,8 @@ def filter_number_list(the_numberlist, criteria_def):
   return(filtered_number_list)
 # block ends
 
-def is_sum_2020(the_first_number, the_second_number):
-  the_sum = the_first_number + the_second_number
+def is_sum_2020(the_first_number, the_second_number, the_third_number):
+  the_sum = the_first_number + the_second_number + the_third_number
   if the_sum == 2020:
     return True
   return False
@@ -51,5 +67,5 @@ results = filter_number_list(numberlist, is_sum_2020)
 
 print(results)
 
-answer = results[0] * results[1] 
-print(answer)
+answer = results[0] * results[1] * results[2]
+print('the answer is', answer)
