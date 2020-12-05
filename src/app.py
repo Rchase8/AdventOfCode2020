02a306635ab3289ac0143d080b18fbb070ee69d1
Rchase8 <chaseerobert@gmail.com>
2020-12-05 21:52:48 +0000

    numbers are no longer lines

--- src/app.py
+++ src/app.py
@@ -1,7 +1,12 @@
 def get_number_list():
   file = open("Data/1_1inputs.txt", "r")
   lines = file.readlines()
-  return lines
+
+  numberlist = []
+  for line in lines:
+    numberlist.append(int(line))
+    
+  return numberlist
 
 
 numberlist = get_number_list()
